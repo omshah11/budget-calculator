f1e21bc88c6715682fda9e195c452c88866f4d69
omshah11 <61638057+omshah11@users.noreply.github.com>
2022-12-27 01:52:34 -0800

    added express shortcuts to get, update, and receive json data

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,71 @@
 import express from 'express';
+import {MongoClient} from 'mongodb'
 
 // PUT /{month-number}
+
+// const months = [
+//     {
+//         month: "1",
+//         year: "2023",
+//         totalExpense: "$259",
+//         content: ["shoes: $59", "clothes: $200"]
+//     },
+//     {
+//         month: "2",
+//         year: "2023",
+//         totalExpense: "$150",
+//         content: ["snacks: $50", "books: $100"]
+//     }
+// ]
 
 const app = express();
 
 app.use(express.json());
 
-app.put('/api/:monthNum', (req,res) => {
+app.get('/api/:year/:month', async (req,res) => {
+    //const {year} = req.params.year;
+    const {year} = req.params;
+    const {month} = req.params;
 
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
+
+    const db = client.db('react-expense-calculator-db');
+
+    const article = await db.collection('articles').findOne({month: month,year : year})
+
+    if (article){
+        res.json(article);
+    } else {
+        res.sendStatus(404).send('No budget found yet!');
+    }
+})
+
+app.post('/api/:year/:month/addExpense', async (req,res) => {
+    //const {year} = req.params.year;
+    const {year} = req.params;
+    const {month} = req.params;
+    const {newExpense, value} = req.body;
+    
+
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
+
+    const db = client.db('react-expense-calculator-db');
+
+    const article = await db.collection('articles').findOne({month: month,year : year})
+    const currTotalExp = article.totalExpense;
+
+    await db.collection('articles').updateOne({year, month}, {
+        $push: {content : newExpense},
+        $set: {totalExpense: String(parseInt(currTotalExp) + parseInt(value))}
+    })
+
+    if (article){
+        res.json(article);
+    } else {
+        res.sendStatus(404).send('No budget found yet!');
+    }
 })
 
 app.listen(8000, () => {
