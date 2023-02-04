ba2798ae02574827ed4a3a9b580129161d410e9c
omshah11 <61638057+omshah11@users.noreply.github.com>
2023-02-03 19:37:45 -0800

    Made progress on adding a delete item form.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,6 @@
 import express from 'express';
 import {MongoClient} from 'mongodb'
 
-// PUT /{month-number}
-
-// const months = [
-//     {
-//         month: "1",
-//         year: "2023",
-//         totalExpense: "$259",
-//         content: ["shoes: $59", "clothes: $200"]
-//     },
-//     {
-//         month: "2",
-//         year: "2023",
-//         totalExpense: "$150",
-//         content: ["snacks: $50", "books: $100"]
-//     }
-// ]
-
 const app = express();
 
 app.use(express.json());
@@ -60,7 +43,32 @@ app.post('/api/:year/:month/addExpense', async (req,res) => {
 
     await db.collection('articles').updateOne({year, month}, {
         $push: {content : {expense, price}},
-        $set: {totalExpense: currTotalExp + price}
+        $set: {totalExpense: parseInt(currTotalExp) + parseInt(price)},
+    })
+
+    if (article){
+        res.json(article);
+    } else {
+        res.sendStatus(404).send('No budget found yet!');
+    }
+})
+
+app.delete('/api/:year/:month/delExpense', async (req,res) => {
+    const {year} = req.params;
+    const {month} = req.params;
+    const {expense} = req.body;
+    console.log(expense);
+    
+    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    await client.connect();
+
+    const db = client.db('react-expense-calculator-db');
+
+    const article = await db.collection('articles').findOne({month: month,year : year})
+    const currTotalExp = article.totalExpense;
+
+    await db.collection('articles').updateOne({year, month}, {
+        $pop: {content : {expense}}
     })
 
     if (article){
