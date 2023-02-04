f2ed9b88e0524c6529cc07931316d4b02d36f121
omshah11 <61638057+omshah11@users.noreply.github.com>
2023-02-04 01:28:56 -0800

    Deployment and Hosting changes
    added firebase authentication, ability to add new data for new months, hosted the database on MongoDB Atlas, and finally deployed and hosted the website on Google Cloud.

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,28 @@
 import express from 'express';
+import path from 'path';
 import {MongoClient} from 'mongodb'
+import {fileURLToPath} from 'url';
+import 'dotenv/config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '../build')));
+
+app.get(/^(?!\/api).+/, (req,res) => {
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+})
 
 app.get('/api/:year/:month', async (req,res) => {
     //const {year} = req.params.year;
     const {year} = req.params;
     const {month} = req.params;
 
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.gpjzfpz.mongodb.net/?retryWrites=true&w=majority`);
     await client.connect();
 
     const db = client.db('react-expense-calculator-db');
@@ -20,7 +32,7 @@ app.get('/api/:year/:month', async (req,res) => {
     if (article){
         res.json(article);
     } else {
-        res.sendStatus(404).send('No budget found yet!');
+        //res.sendStatus(404).send('No budget found yet!');
     }
 })
 
@@ -33,24 +45,44 @@ app.post('/api/:year/:month/addExpense', async (req,res) => {
     console.log(price);
     
 
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.gpjzfpz.mongodb.net/?retryWrites=true&w=majority`);
     await client.connect();
 
     const db = client.db('react-expense-calculator-db');
 
     const article = await db.collection('articles').findOne({month: month,year : year})
-    const currTotalExp = article.totalExpense;
-
-    await db.collection('articles').updateOne({year, month}, {
-        $push: {content : {expense, price}},
-        $set: {totalExpense: parseInt(currTotalExp) + parseInt(price)},
-    })
-
-    if (article){
-        res.json(article);
-    } else {
-        res.sendStatus(404).send('No budget found yet!');
+    console.log(article)
+    if (article != null){
+        const currTotalExp = article.totalExpense;
+        await db.collection('articles').updateOne({year, month}, {
+            $push: {content : {expense, price}},
+            $set: {totalExpense: parseInt(currTotalExp) + parseInt(price)}
+        })
     }
+    else {
+        //const currTotalExp = article.totalExpense;
+        console.log("inside");
+        await db.collection('articles').insert(
+            {
+                "month": month,
+                "year": year,
+                "totalExpense": parseInt(price),
+                "content": [
+                  {
+                    "expense": expense,
+                    "price": parseInt(price)
+                  }
+                ]
+            }
+            )
+        //console.log(totalExpense)
+    }
+    
+    // if (article){
+    //     res.json(article);
+    // } else {
+    //     res.sendStatus(404).send('No budget found yet!');
+    // }
 })
 
 app.delete('/api/:year/:month/delExpense', async (req,res) => {
@@ -59,7 +91,7 @@ app.delete('/api/:year/:month/delExpense', async (req,res) => {
     const {expense} = req.body;
     console.log(expense);
     
-    const client = new MongoClient('mongodb://127.0.0.1:27017');
+    const client = new MongoClient(`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASSWORD}@cluster0.gpjzfpz.mongodb.net/?retryWrites=true&w=majority`);
     await client.connect();
 
     const db = client.db('react-expense-calculator-db');
@@ -78,7 +110,9 @@ app.delete('/api/:year/:month/delExpense', async (req,res) => {
     }
 })
 
+const PORT = process.env.PORT || 8000;
+
 app.listen(8000, () => {
-    console.log('Server is listening on port 8000');
+    console.log('Server is listening on port ' + PORT);
 });
 
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,6 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBMCFV04zkvBc6dDIyBx_IrijkcZ3dSlPY",
+  authDomain: "my-react-expense-calculator.firebaseapp.com",
+  projectId: "my-react-expense-calculator",
+  storageBucket: "my-react-expense-calculator.appspot.com",
+  messagingSenderId: "814120390027",
+  appId: "1:814120390027:web:d2c1eab3f0139f45ce338e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
